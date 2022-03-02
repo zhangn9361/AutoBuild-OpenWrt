5ac6667c799e2f02cbf99f1872f12b015ebc4ce6
zhangn9361 <zhangn9361@163.com>
2022-03-02 08:21:10 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -8,8 +8,11 @@
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 
-#2. Clear the login password
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+#2. 修改主机名
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWRT'' openwrt/package/lean/default-settings/files/zzz-default-settings
 
-#3. Replace with JerryKuKu’s Argon
-#rm openwrt/package/lean/luci-theme-argon -rf
+#3. 版本号里显示一个自己的名字
+sed -i "s/OpenWrt /zNing build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" openwrt/package/lean/default-settings/files/zzz-default-settings
+
+#4. Replace with JerryKuKu’s Argon
+rm openwrt/package/lean/luci-theme-argon -rf
